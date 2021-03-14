11e0e50e72c5fe0a56fe152880e67bd582ed0fa5
Ryomen-Sukuna <ryomensukuna83@gmail.com>
2021-03-14 10:31:02 +0700

    add pyrogram
    Signed-off-by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -3,6 +3,7 @@ import time
 import re
 from sys import argv
 from typing import Optional
+from pyrogram import idle, Client
 
 from SaitamaRobot import (
     ALLOW_EXCL,
@@ -20,6 +21,7 @@ from SaitamaRobot import (
     StartTime,
     telethn,
     updater,
+    app
 )
 
 # needed to dynamically load modules
@@ -668,3 +670,4 @@ if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
     main()
+    idle()
--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -1,12 +1,45 @@
-import time
 from telethon import events
+from pyrogram import filters
+from SaitamaRobot import app
+import asyncio
+from pyrogram import raw
 
-from SaitamaRobot import telethn
-from SaitamaRobot.modules.helper_funcs.telethn.chatstatus import (
-    can_delete_messages,
-    user_is_admin,
-)
+async def user_can_purge(_, c, m):
+    mem = await c.get_chat_member(chat_id=m.chat.id, user_id=m.from_user.id)
+    if mem.can_delete_messages or mem.status in ("creator"):
+        return True
+    await m.reply("You Don't Have Permission To Delete Messages")
 
+can_purge = filters.create(user_can_purge)
+
+@app.on_message(filters.command("purge")& filters.reply & can_purge, group=0)
+async def purge(c, m):
+    messages = list(range(m.message_id + 1, m.reply_to_message.message_id, -1))
+    while len(messages) != 0:
+        await c.send(
+            raw.functions.channels.DeleteMessages(
+                channel=await c.resolve_peer(m.chat.id), id=messages))
+        del messages[0:100]
+    await c.send_message(
+        text="**Fast Purge Completed!!!**",
+        chat_id=m.chat.id)
+
+@app.on_message(filters.command('purge')& ~filters.reply & can_purge)
+async def int_purge(c, m):
+    try:
+        count = int(m.command[1])
+    except IndexError:
+        await m.reply("__specifiy number of messages to delete__")
+        return
+    messages = list(range(m.message_id - 1, m.message_id - 1 - count, -1))
+    start, end = messages[-1], messages[0]
+    while len(messages) != 0:
+        await c.send(
+            raw.functions.channels.DeleteMessages(
+                channel=await c.resolve_peer(m.chat.id), id=messages))
+        del messages[0:100]
+    await c.send_message(
+        text=f"Purged {count} messages from {start} to {end}",chat_id=m.chat.id)
 
 async def purge_messages(event):
     start = time.perf_counter()
