6d0fa09b6686b8119d518c01e91302d5ed051eed
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-14 22:49:29 +0700

    Update gtranslator.py

--- SaitamaRobot/modules/gtranslator.py
+++ SaitamaRobot/modules/gtranslator.py
@@ -49,17 +49,16 @@ if __name__ == "__translate__":
 
 __help__ = f"""
 • `/tr` or `/tl` (language code) as reply to a long message.
-     *Example:* `/tr en`*:* translates something to english.
-         `/tr ja-en`*:* translates Japanese to english.
-
-     *Lang Codes available:*
-     `
-     af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
-     et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
-     id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
-     ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
-     sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
-     vi,xh,yi,yo,zh,zh_CN,zh_TW,zu
+*Example:* `/tr en`*:* translates something to english.
+`/tr ja-en`*:* translates Japanese to english.
+*Lang Codes available:*
+`
+af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
+id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+vi,xh,yi,yo,zh,zh_CN,zh_TW,zu
      `
 """
 
