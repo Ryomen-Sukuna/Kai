316ef4472c21724034b8021fbb8ccbfddb6c80bf
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-14 18:52:57 +0700

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -17,70 +17,70 @@ def get_user_list(__init__, key):
         return json.load(json_file)[key]
 
 # enable logging
-FORMAT = "[ShieHashaikai] %(message)s"
+FORMAT = "[Enterprise] %(message)s"
 logging.basicConfig(handlers=[RichHandler()], level=logging.INFO, format=FORMAT, datefmt="[%X]")
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 log = logging.getLogger("rich")
 
-log.info("[KAI] Kai is starting. | An Zero Union Project. | Licensed under GPLv3.")
+log.info("[KIGYO] Kigyo is starting. | An Eagle Union Project. | Licensed under GPLv3.")
 
-log.info("[KAI] Not affiliated to Shie Hashaikai or Vilain in any way whatsoever.")
-log.info("[KAI] Project maintained by: github.com/ChisakiKai (t.me/Anomaliii)")
+log.info("[KIGYO] Not affiliated to Azur Lane or Yostar in any way whatsoever.")
+log.info("[KIGYO] Project maintained by: github.com/Dank-del (t.me/dank_as_fuck)")
 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 7:
     log.error(
-        "[KAI] You MUST have a python version of at least 3.7! Multiple features depend on this. Bot quitting."
+        "[KIGYO] You MUST have a python version of at least 3.7! Multiple features depend on this. Bot quitting."
     )
     quit(1)
 
 parser = ConfigParser()
 parser.read("config.ini")
-kaiconfig = parser["kaiconfig"]
+kigconfig = parser["kigconfig"]
 
 
-OWNER_ID = kaiconfig.getint("OWNER_ID")
-OWNER_USERNAME = kaiconfig.get("OWNER_USERNAME")
-APP_ID = kaiconfig.getint("APP_ID")
-API_HASH = kaiconfig.get("API_HASH")
-WEBHOOK = kaiconfig.getboolean("WEBHOOK", False)
-URL = kaiconfig.get("URL", None)
-CERT_PATH = kaiconfig.get("CERT_PATH", None)
-PORT = kaiconfig.getint("PORT", None)
-INFOPIC = kaiconfig.getboolean("INFOPIC", False)
-DEL_CMDS = kaiconfig.getboolean("DEL_CMDS", False)
-STRICT_GBAN = kaiconfig.getboolean("STRICT_GBAN", False)
-ALLOW_EXCL = kaiconfig.getboolean("ALLOW_EXCL", False)
-CUSTOM_CMD = kaiconfig.get("CUSTOM_CMD", None)
-BAN_STICKER = kaiconfig.get("BAN_STICKER", None)
-TOKEN = kaiconfig.get("TOKEN")
-DB_URI = kaiconfig.get("SQLALCHEMY_DATABASE_URI")
-LOAD = kaiconfig.get("LOAD").split()
+OWNER_ID = kigconfig.getint("OWNER_ID")
+OWNER_USERNAME = kigconfig.get("OWNER_USERNAME")
+APP_ID = kigconfig.getint("APP_ID")
+API_HASH = kigconfig.get("API_HASH")
+WEBHOOK = kigconfig.getboolean("WEBHOOK", False)
+URL = kigconfig.get("URL", None)
+CERT_PATH = kigconfig.get("CERT_PATH", None)
+PORT = kigconfig.getint("PORT", None)
+INFOPIC = kigconfig.getboolean("INFOPIC", False)
+DEL_CMDS = kigconfig.getboolean("DEL_CMDS", False)
+STRICT_GBAN = kigconfig.getboolean("STRICT_GBAN", False)
+ALLOW_EXCL = kigconfig.getboolean("ALLOW_EXCL", False)
+CUSTOM_CMD = kigconfig.get("CUSTOM_CMD", None)
+BAN_STICKER = kigconfig.get("BAN_STICKER", None)
+TOKEN = kigconfig.get("TOKEN")
+DB_URI = kigconfig.get("SQLALCHEMY_DATABASE_URI")
+LOAD = kigconfig.get("LOAD").split()
 LOAD = list(map(str, LOAD))
-MESSAGE_DUMP = kaiconfig.getfloat("MESSAGE_DUMP")
-GBAN_LOGS = kaiconfig.getfloat("GBAN_LOGS")
-NO_LOAD = kaiconfig.get("NO_LOAD").split()
+MESSAGE_DUMP = kigconfig.getfloat("MESSAGE_DUMP")
+GBAN_LOGS = kigconfig.getfloat("GBAN_LOGS")
+NO_LOAD = kigconfig.get("NO_LOAD").split()
 NO_LOAD = list(map(str, NO_LOAD))
-DRAGONS = get_user_list("elevated_users.json", "sudos")
+SUDO_USERS = get_user_list("elevated_users.json", "sudos")
 DEV_USERS = get_user_list("elevated_users.json", "devs")
-DEMON = get_user_list("elevated_users.json", "supports")
-TIGERS = get_user_list("elevated_users.json", "demons")
-WOLVES = get_user_list("elevated_users.json", "tigers")
-SPAMMERS = get_user_list("elevated_users.json", "wolves")
-spamwatch_api = kaiconfig.get("spamwatch_api")
-CASH_API_KEY = kaiconfig.get("CASH_API_KEY")
-TIME_API_KEY = kaiconfig.get("TIME_API_KEY")
-WALL_API = kaiconfig.get("WALL_API")
-LASTFM_API_KEY = kaiconfig.get("LASTFM_API_KEY")
+SUPPORT_USERS = get_user_list("elevated_users.json", "supports")
+SARDEGNA_USERS = get_user_list("elevated_users.json", "sardegnas")
+WHITELIST_USERS = get_user_list("elevated_users.json", "whitelists")
+SPAMMERS = get_user_list("elevated_users.json", "spammers")
+spamwatch_api = kigconfig.get("spamwatch_api")
+CASH_API_KEY = kigconfig.get("CASH_API_KEY")
+TIME_API_KEY = kigconfig.get("TIME_API_KEY")
+WALL_API = kigconfig.get("WALL_API")
+LASTFM_API_KEY = kigconfig.get("LASTFM_API_KEY")
 try:
-    CF_API_KEY = kaiconfig.get("CF_API_KEY")
+    CF_API_KEY = kigconfig.get("CF_API_KEY")
     log.info("[NLP] AI antispam powered by Intellivoid.")
 except:
     log.info("[NLP] No Coffeehouse API key provided.")
     CF_API_KEY = None
 
 
-DRAGONS.append(OWNER_ID)
+SUDO_USERS.append(OWNER_ID)
 DEV_USERS.append(OWNER_ID)
 
 # SpamWatch
@@ -95,10 +95,10 @@ else:
         log.warning("Can't connect to SpamWatch!")
 
 updater = tg.Updater(TOKEN, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10})
-telethn = TelegramClient("kai", API_ID, API_HASH)
+telethn = TelegramClient("kigyo", APP_ID, API_HASH)
 dispatcher = updater.dispatcher
 
-kp = Client("KaiPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, workers=min(32, os.cpu_count() + 4))
+kp = Client("KigyoPyro", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN, workers=min(32, os.cpu_count() + 4))
 apps = []
 apps.append(kp)
 
@@ -130,23 +130,19 @@ async def get_entity(client, entity):
     return entity, entity_client
 
 
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
+SUDO_USERS = list(SUDO_USERS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
-WOLVES = list(WOLVES)
-DEMONS = list(DEMONS)
-TIGERS = list(TIGERS)
+WHITELIST_USERS = list(WHITELIST_USERS)
+SUPPORT_USERS = list(SUPPORT_USERS)
+SARDEGNA_USERS = list(SARDEGNA_USERS)
+SPAMMERS = list(SPAMMERS)
 
 # Load at end to ensure all prev variables have been set
-from SaitamaRobot.modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomMessageHandler,
-    CustomRegexHandler,
-)
+from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler
 
-# make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
-tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
+if CUSTOM_CMD and len(CUSTOM_CMD) >= 1:
+    tg.CommandHandler = CustomCommandHandler
+
 
 def spamfilters(text, user_id, chat_id):
     # print("{} | {} | {}".format(text, user_id, chat_id))
@@ -155,4 +151,3 @@ def spamfilters(text, user_id, chat_id):
         return True
     else:
         return False
-
