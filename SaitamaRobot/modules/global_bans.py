73896956cc3777ef8333545b07204496c3a9a374
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-14 16:30:11 +0700

    Update global_bans.py
    Signed-off by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- SaitamaRobot/modules/global_bans.py
+++ SaitamaRobot/modules/global_bans.py
@@ -575,7 +575,7 @@ def __migrate__(old_chat_id, new_chat_id):
 
 
 def __chat_settings__(chat_id, user_id):
-    return f"This chat is enforcing *gbans*: `{sql.does_chat_gban(chat_id)}`.
+    return f"This chat is enforcing *gbans*: `{sql.does_chat_gban(chat_id)}`."
     
     
 __help__ =f"""
