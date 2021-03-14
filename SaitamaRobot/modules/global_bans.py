408aa9d56b8080dbd227f343fc8b35b1de6787d5
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-14 16:12:01 +0700

    Update global_bans.py
    Signed-off by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- SaitamaRobot/modules/global_bans.py
+++ SaitamaRobot/modules/global_bans.py
@@ -6,16 +6,17 @@ from SaitamaRobot.modules.sql.users_sql import get_user_com_chats
 import SaitamaRobot.modules.sql.global_bans_sql as sql
 from SaitamaRobot import (
     DEV_USERS,
-    GBAN_LOGS,
+    EVENT_LOGS,
     OWNER_ID,
     STRICT_GBAN,
-    SUDO_USERS,
-    SUPPORT_USERS,
-    SARDEGNA_USERS,
-    WHITELIST_USERS,
+    DRAGONS,
+    SUPPORT_CHAT,
+    SPAMWATCH_SUPPORT_CHAT,
+    DEMONS,
+    TIGERS,
+    WOLVES,
     sw,
     dispatcher,
-    log
 )
 from SaitamaRobot.modules.helper_funcs.chat_status import (
     is_user_admin,
