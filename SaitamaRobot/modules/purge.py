73f5d874b70e1b4f093d4f933a5ca24faa1c0cb0
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-17 22:55:31 +0700

    Update purge.py
    Signed-off by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from tg_bot import telethn
-from tg_bot.modules.helper_funcs.telethn.chatstatus import (
+from SaitamaRobot import telethn
+from SaitamaRobot.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages, user_is_admin)
 
 
