a07799f70c633882be590b820265ddc355ef9cbc
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-18 01:22:37 +0700

    Update __init__.py
    Signed-off by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- SaitamaRobot/modules/sql/__init__.py
+++ SaitamaRobot/modules/sql/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from SaitamaRobot import DB_URI, logging
+from SaitamaRobot import DB_URI, LOGGER
 
 
 def start() -> scoped_session:
