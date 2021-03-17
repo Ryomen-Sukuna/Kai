b488fb460bd45a102377293b30fbc2f551860f4f
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-17 22:05:24 +0700

    Update __init__.py

--- SaitamaRobot/modules/sql/__init__.py
+++ SaitamaRobot/modules/sql/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(postgres://vgmeibvvndsmoq:143f140f2bf05f92f0fb23547d0dfe1546c41cb37b7201abf9c20fd0eb27ea1a@ec2-34-200-106-49.compute-1.amazonaws.com:5432/d9orbc0m4t896k, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
