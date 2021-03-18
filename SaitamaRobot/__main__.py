739a8f4bd48487083761a0e814bb8d0edc101e47
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-18 20:50:59 +0700

    Update __main__.py
    Signed-off by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -235,7 +235,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "I'm Here!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            "Yo, I'm Here!\n<b>Alive since:</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
