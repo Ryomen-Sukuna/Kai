4f2b8bd971bcebfbccad79aced11520524b6aa8a
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-20 15:51:55 +0700

    edit core
    Signed-off by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -290,7 +290,7 @@ def help_button(update, context):
             query.message.edit_text(
                 text=text,
                 parse_mode=ParseMode.MARKDOWN,
-                disable_web_page_preview=True,
+                disable_web_page_preview=False,
                 reply_markup=InlineKeyboardMarkup(
                     [[InlineKeyboardButton(text="Back", callback_data="help_back")]]
                 ),
