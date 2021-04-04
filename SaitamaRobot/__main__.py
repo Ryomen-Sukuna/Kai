acc6d0cb988e46e73b3aa9a57b08ad4e6de7dd01
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-04-04 12:02:51 +0700

    change help
    Signed-off-by: Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -94,12 +94,8 @@ I'm an Anime themed group management Bot to help admins manage their groups! Hav
  • /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
-{}
-And the following:
-""".format(
-    dispatcher.bot.first_name,
-    "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n",
-)
+\nClick on the buttons below to get documentation about specific modules!"""
+
 
 KAI_IMG = "https://telegra.ph/file/805d67c52c6ee73003d1e.jpg"
 
