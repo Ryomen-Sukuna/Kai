ad0934c3afb845f742da971277bdd05bbb37fd4b
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-07 02:18:05 +0700

    rework translator

--- SaitamaRobot/modules/gtranslator.py
+++ SaitamaRobot/modules/gtranslator.py
@@ -1,54 +1,21 @@
 from gpytranslate import Translator
-import asyncio
-from SaitamaRobot import kp
+from telegram.ext import CommandHandler, CallbackContext
+from telegram import (
+    Message,
+    Chat,
+    User,
+    ParseMode,
+    Update,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
+from SaitamaRobot import dispatcher, kp
 from pyrogram import filters
+from pyrogram.types import Message
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 
 
-@kp.on_message(filters.command(["tr", "tl"], prefixes=["/", "!", "."]))
-async def translate(_client, message):
-    trl = Translator()
-    if message.reply_to_message and (
-        message.reply_to_message.text or message.reply_to_message.caption
-    ):
-        if len(message.text.split()) == 1:
-            await message.reply_text(
-                "Provide lang code.\n[Available options](https://telegra.ph/Lang-Codes-11-08).\n**Usage:** `/tr en`"
-            )
-            return
-        target = message.text.split()[1]
-        if message.reply_to_message.text:
-            text = message.reply_to_message.text
-        else:
-            text = message.reply_to_message.caption
-        detectlang = await trl.detect(text)
-        try:
-            tekstr = await trl(text, targetlang=target)
-        except ValueError as err:
-            await message.reply_text(f"Error: `{str(err)}`")
-            return
-    else:
-        if len(message.text.split()) <= 2:
-            await message.reply_text(
-                "Provide lang code.\n[Available options](https://telegra.ph/Lang-Codes-11-08).\n**Usage:** `/tr en`"
-            )
-            return
-        target = message.text.split(None, 2)[1]
-        text = message.text.split(None, 2)[2]
-        detectlang = await trl.detect(text)
-        try:
-            tekstr = await trl(text, targetlang=target)
-        except ValueError as err:
-            await message.reply_text("Error: `{}`".format(str(err)))
-            return
-
-    await message.reply_text(f"**Translated:**\n```{tekstr.text}```")
-
-
-if __name__ == "__translate__":
-    asyncio.run(translate())
-
-__help__ = f"""
-Use this module to translate stuff... duh!
+Use this module to translate stuff!
 *Commands:*
 • `/tl` (or `/tr`): as a reply to a message, translates it to English.
 • `/tl <lang>`: translates to <lang>
@@ -56,7 +23,58 @@ eg: `/tl ja`: translates to Japanese.
 • `/tl <source>//<dest>`: translates from <source> to <lang>.
 eg: `/tl ja//en`: translates from Japanese to English.
 • `/langs`: get a list of supported languages for translation.
-"""
 
 __mod_name__ = "Translator"
-__command_list__ = ["tr", "tl"]
+
+
+trans = Translator()
+
+
+@kp.on_message(filters.command(["tl", "tr"]))
+async def translate(_, message: Message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("Reply to a message to translate it!")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = await trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await trans.detect(to_translate)
+        dest = "en"
+    translation = await trans(to_translate,
+                              sourcelang=source, targetlang=dest)
+    reply = f"<b>Translated from {source} to {dest}</b>:\n" \
+        f"<code>{translation.text}</code>"
+
+    await message.reply_text(reply, parse_mode="html")
+
+def languages(update: Update, context: CallbackContext) -> None:
+    update.effective_message.reply_text(
+        "Click on the button below to see the list of supported language codes.",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Language codes",
+                        url="https://telegra.ph/Lang-Codes-03-19-3"
+                        ),
+                ],
+            ],
+        disable_web_page_preview=True
+    )
+        )
+
+
+LANG_HANDLER = DisableAbleCommandHandler("langs", languages, run_async=True)
+
+dispatcher.add_handler(LANG_HANDLER)
