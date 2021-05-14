3f6a483cca7d923129e2d264687d1bba67dcabbb
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-14 22:33:04 +0700

    trying fix stickers

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -41,20 +41,20 @@ def stickerid(update: Update, context: CallbackContext):
 @run_async
 def cb_sticker(update: Update, context: CallbackContext):
     msg = update.effective_message
-    split = msg.text.split(" ", 1)
+    split = msg.text.split(' ', 1)
     if len(split) == 1:
-        msg.reply_text("Provide some name to search for pack.")
+        msg.reply_text('Provide some name to search for pack.')
         return
     text = requests.get(combot_stickers_url + split[1]).text
-    soup = bs(text, "lxml")
-    results = soup.find_all("a", {"class": "sticker-pack__btn"})
+    soup = bs(text, 'lxml')
+    results = soup.find_all("a", {'class': "sticker-pack__btn"})
     titles = soup.find_all("div", "sticker-pack__title")
     if not results:
-        msg.reply_text("No results found :(.")
+        msg.reply_text('No results found :(.')
         return
     reply = f"Stickers for *{split[1]}*:"
     for result, title in zip(results, titles):
-        link = result["href"]
+        link = result['href']
         reply += f"\n• [{title.get_text()}]({link})"
     msg.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
 
