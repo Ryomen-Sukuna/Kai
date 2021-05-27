e26c3fa3a3747eb09c7bb696286f8d6b7f1503e8
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-28 00:24:53 +0700

    trying add voice chat
    Signed-off by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

---
+++ SaitamaRobot/modules/vc.py
@@ -0,0 +1,18 @@
+__help__ = """
+ • /playm <reply to song u want to play in vc>:Add @TESTMUSIC in group and play song u want to play in vc
+
+ • /stopm <to stop the song playing in vc> : Stop the song playing in vc
+
+ • /skip To skip to the next song 
+
+ • /ytp <similar to playm but don't need to reply the song> Play song from YouTube
+
+ • /deezer <get your song>
+
+ • IMPORTANT REQUIREMENTS
+
+ `Add @TESTMUSIC in group before playing any song else this will not work'
+ 
+"""
+
+__mod_name__ = "Voice-Chat"
