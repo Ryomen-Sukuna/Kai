755baad32ef512608124193e3541a3a089266b0e
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-28 03:21:34 +0700

    F
    Signed-off by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- SaitamaRobot/modules/karma.py
+++ SaitamaRobot/modules/karma.py
@@ -5,17 +5,14 @@ import asyncio
 from pyrogram import filters
 
 from SaitamaRobot import kp
-from SaitamaRobot.pyroutils.permissions import adminsOnly
 from SaitamaRobot.pyroutils.errors import capture_err
-from SaitamaRobot.utils.dbfunctions import (alpha_to_int, get_karma, get_karmas,
-                                   int_to_alpha, is_karma_on, karma_off,
-                                   karma_on, update_karma)
+from SaitamaRobot.utils.dbfunctions import (update_karma, get_karma, get_karmas,
+                                   int_to_alpha, alpha_to_int)
 from SaitamaRobot.utils.filter_groups import karma_negative_group, karma_positive_group
 
 __MODULE__ = "Karma"
 __HELP__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
 [DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
-/karma_toggle [ENABLE|DISABLE] - Enable or Disable Karma System In Your Chat.
 Reply to a message with /karma to check a user's karma
 Send /karma without replying to any message to chek karma list of top 10 users"""
 
@@ -155,21 +152,3 @@ async def karma(_, message):
         else:
             karma = 0
             await message.reply_text(f"**Total Points**: __{karma}__")
-
-@kp.on_message(filters.command("karma_toggle") & ~filters.private)
-@adminsOnly("can_change_info")
-async def captcha_state(_, message):
-    usage = "**Usage:**\n/karma_toggle [ENABLE|DISABLE]"
-    if len(message.command) != 2:
-        return await message.reply_text(usage)
-    chat_id = message.chat.id
-    state = message.text.split(None, 1)[1].strip()
-    state = state.lower()
-    if state == "enable":
-        await karma_on(chat_id)
-        await message.reply_text("Enabled karma system.")
-    elif state == "disable":
-        await karma_off(chat_id)
-        await message.reply_text("Disabled karma system.")
-    else:
-        await message.reply_text(usage)
