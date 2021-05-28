a47b79758b2b68162a7d381d38c5cb477b94b9b4
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-28 12:17:01 +0700

    Update karma.py

--- SaitamaRobot/modules/karma.py
+++ SaitamaRobot/modules/karma.py
@@ -1,4 +1,4 @@
-from SaitamaRobot import pgram
+from SaitamaRobot import kp
 from SaitamaRobot.utils.dbfunctions import (update_karma, get_karma, get_karmas,
                                    int_to_alpha, alpha_to_int)
 from SaitamaRobot.utils.filter_groups import karma_positive_group, karma_negative_group
