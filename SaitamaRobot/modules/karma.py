bce5c93613ade0bf4704c1130e1ccd908d1b2c9a
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-30 00:05:35 +0700

    add help menu on karma
    Thanks to HeLLxGodLike for help <https://github.com/HellxGodLike>
    
    Signed-off by: Ryomen-Sukuna <ryomensukun83@gmail.com>

--- SaitamaRobot/modules/karma.py
+++ SaitamaRobot/modules/karma.py
@@ -11,8 +11,8 @@ ARQ_API_KEY= "YKYUHE-KEWVTL-HHSTVX-AZDKWX-ARQ"
 ARQ_API_URL = "https://thearq.tech"
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
-__MODULE__ = "Karma"
-__HELP__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
+__mod_name__ = "Karma"
+__help__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
 [DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
 Reply to a message with /karma to check a user's karma
 Send /karma without replying to any message to chek karma list of top 10 users"""
