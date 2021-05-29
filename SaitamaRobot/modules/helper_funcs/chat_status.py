a23c87547cae319e867eed926daec10b28c9eec4
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-29 18:35:31 +0700

    change little
    Signed-off by: Ryomen-Sukuna <ryomensukun83@gmail.com>

--- SaitamaRobot/modules/helper_funcs/chat_status.py
+++ SaitamaRobot/modules/helper_funcs/chat_status.py
@@ -252,9 +252,9 @@ def bot_admin(func):
         message_chat_title = update.effective_message.chat.title
 
         if update_chat_title == message_chat_title:
-            not_admin = "I'm not admin! - REEEEEE"
+            not_admin = "I'm not admin!"
         else:
-            not_admin = f"I'm not admin in <b>{update_chat_title}</b>! - REEEEEE"
+            not_admin = f"I'm not admin in <b>{update_chat_title}</b>!"
 
         if is_bot_admin(chat, bot.id):
             return func(update, context, *args, **kwargs)
