152d662aa2a2291c7b714e2ea5b531c604ad2a83
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-30 16:50:19 +0700

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -12,8 +12,8 @@ from google_trans_new import google_translator
 from pyrogram import filters
 
 from SaitamaRobot import BOT_ID
-from SaitamaRobot.db.mongo_helpers.aichat import add_chat, get_session, remove_chat
-from SaitamaRobot.function.pluginhelpers import admins_only, edit_or_reply
+from SaitamaRobot.sql.chatbot_sql import add_chat, get_session, remove_chat
+from SaitamaRobot.utils.adminperms import admins_only, edit_or_reply
 from SaitamaRobot.modules.karma import arq
 from SaitamaRobot import kp as eugen
 
