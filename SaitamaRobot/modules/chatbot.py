1ddd0943c2528f1ac885c9a79ad9ee4edb7c2e0e
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-30 16:17:42 +0700

    F

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -1,66 +1,384 @@
-from SaitamaRobot import kp, DRAGONS
-from SaitamaRobot.utils.errors import capture_err
-from SaitamaRobot.utils.filter_groups import chatbot_group
+import re
+
+import emoji
+
+IBM_WATSON_CRED_URL = "https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/bd6b59ba-3134-4dd4-aff2-49a79641ea15"
+IBM_WATSON_CRED_PASSWORD = "UQ1MtTzZhEsMGK094klnfa-7y_4MCpJY1yhd52MXOo3Y"
+url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
+import re
+
+import aiohttp
+from google_trans_new import google_translator
 from pyrogram import filters
-from SaitamaRobot.modules.karma import arq
 
-BOT_ID = 1820343887
+from PythonARQ import arq
+from SaitamaRobot import BOT_ID
+from SaitamaRobot.db.mongo_helpers.aichat import add_chat, get_session, remove_chat
+from SaitamaRobot.function.pluginhelpers import admins_only, edit_or_reply
+from SaitamaRobot import kp as eugen
 
-__mod_name__ = "Chatbot"
-__help__ = """
-/chatbot [ON|OFF] To Enable Or Disable Chatbot In Your Chat.
+translator = google_translator()
+
+
+async def lunaQuery(query: str, user_id: int):
+    luna = await arq.luna(query, user_id)
+    return luna.result
+
+
+def extract_emojis(s):
+    return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
+
+
+async def fetch(url):
+    try:
+        async with aiohttp.Timeout(10.0):
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    try:
+                        data = await resp.json()
+                    except:
+                        data = await resp.text()
+            return data
+    except:
+        print("AI response Timeout")
+        return
+
+
+daisy_chats = []
+en_chats = []
+# AI Chat (C) 2020-2021 by @InukaAsith
+"""
+@eugen.on_message(
+    filters.voice & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
+    group=2,
+)
+async def hmm(client, message):
+    if not get_session(int(message.chat.id)):
+        message.continue_propagation()
+    if message.reply_to_message.from_user.id != BOT_ID:
+        message.continue_propagation()
+    previous_message = message
+    required_file_name = message.download()
+    if IBM_WATSON_CRED_URL is None or IBM_WATSON_CRED_PASSWORD is None:
+        await message.reply(
+            "You need to set the required ENV variables for this module. \nModule stopping"
+        )
+    else:
+        headers = {
+            "Content-Type": previous_message.voice.mime_type,
+        }
+        data = open(required_file_name, "rb").read()
+        response = requests.post(
+            IBM_WATSON_CRED_URL + "/v1/recognize",
+            headers=headers,
+            data=data,
+            auth=("apikey", IBM_WATSON_CRED_PASSWORD),
+        )
+        r = response.json()
+        print(r)
+        await client.send_message(message, r)
 """
 
-active_chats_bot = []
-active_chats_ubot = []
 
-
-# Enabled | Disable Chatbot
-
-
-@kp.on_message(filters.command("chatbot") & ~filters.edited)
-@capture_err
-async def chatbot_status(_, message):
-    global active_chats_bot
+@eugen.on_message(
+    filters.command("chatbot") & ~filters.edited & ~filters.bot & ~filters.private
+)
+@admins_only
+async def hmm(_, message):
+    global daisy_chats
     if len(message.command) != 2:
-        await message.reply_text("**Usage**\n/chatbot [ON|OFF]")
-        return
+        await message.reply_text(
+            "I only recognize `/chatbot on` and /chatbot `off only`"
+        )
+        message.continue_propagation()
     status = message.text.split(None, 1)[1]
     chat_id = message.chat.id
-
     if status == "ON" or status == "on" or status == "On":
-        if chat_id not in active_chats_bot:
-            active_chats_bot.append(chat_id)
-            text = "Chatbot Enabled Reply To Any Message " \
-                   + "Of Mine To Get A Reply"
-            await message.reply_text(text)
+        lel = await edit_or_reply(message, "`Processing...`")
+        lol = add_chat(int(message.chat.id))
+        if not lol:
+            await lel.edit("Eugen Already Activated In This Chat")
             return
-        await message.reply_text("ChatBot Is Already Enabled.")
-        return
+        await lel.edit(
+            f"Eugen Successfully Added For Users In The Chat {message.chat.id}"
+        )
 
     elif status == "OFF" or status == "off" or status == "Off":
-        if chat_id in active_chats_bot:
-            active_chats_bot.remove(chat_id)
-            await message.reply_text("Chatbot Disabled!")
+        lel = await edit_or_reply(message, "`Processing...`")
+        Escobar = remove_chat(int(message.chat.id))
+        if not Escobar:
+            await lel.edit(Eugen Was Not Activated In This Chat")
             return
-        await message.reply_text("ChatBot Is Already Disabled.")
-        return
+        await lel.edit(
+            f"Eugen Successfully Deactivated For Users In The Chat {message.chat.id}"
+        )
 
+    elif status == "EN" or status == "en" or status == "english":
+        if not chat_id in en_chats:
+            en_chats.append(chat_id)
+            await message.reply_text("English AI chat Enabled!")
+            return
+        await message.reply_text("AI Chat Is Already Disabled.")
+        message.continue_propagation()
     else:
-        await message.reply_text("**Usage**\n/chatbot [ON|OFF]")
+        await message.reply_text(
+            "I only recognize `/chatbot on` and /chatbot `off only`"
+        )
 
 
-@kp.on_message(filters.text & filters.reply & ~filters.bot &
-                ~filters.via_bot & ~filters.forwarded, group=chatbot_group)
-@capture_err
-async def chatbot_talk(_, message):
-    if message.chat.id not in active_chats_bot:
+@eugen.on_message(
+    filters.text
+    & filters.reply
+    & ~filters.bot
+    & ~filters.edited
+    & ~filters.via_bot
+    & ~filters.forwarded,
+    group=2,
+)
+async def hmm(client, message):
+    if not get_session(int(message.chat.id)):
         return
     if not message.reply_to_message:
         return
-    if message.reply_to_message.from_user.id != BOT_ID:
+    try:
+        senderr = message.reply_to_message.from_user.id
+    except:
         return
-    query = message.text
-    luna = await arq.luna(query)
-    response = luna.response
-    await message.reply_text(response)
+    if senderr != BOT_ID:
+        return
+    msg = message.text
+    chat_id = message.chat.id
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    if chat_id in en_chats:
+        test = msg
+        test = test.replace("daisy", "Aco")
+        test = test.replace("Daisy", "Aco")
+        response = await lunaQuery(
+            test, message.from_user.id if message.from_user else 0
+        )
+        response = response.replace("Aco", "Daisy")
+        response = response.replace("aco", "Daisy")
+
+        pro = response
+        try:
+            await daisyx.send_chat_action(message.chat.id, "typing")
+            await message.reply_text(pro)
+        except CFError:
+            return
+
+    else:
+        u = msg.split()
+        emj = extract_emojis(msg)
+        msg = msg.replace(emj, "")
+        if (
+            [(k) for k in u if k.startswith("@")]
+            and [(k) for k in u if k.startswith("#")]
+            and [(k) for k in u if k.startswith("/")]
+            and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+        ):
+
+            h = " ".join(filter(lambda x: x[0] != "@", u))
+            km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+            tm = km.split()
+            jm = " ".join(filter(lambda x: x[0] != "#", tm))
+            hm = jm.split()
+            rm = " ".join(filter(lambda x: x[0] != "/", hm))
+        elif [(k) for k in u if k.startswith("@")]:
+
+            rm = " ".join(filter(lambda x: x[0] != "@", u))
+        elif [(k) for k in u if k.startswith("#")]:
+            rm = " ".join(filter(lambda x: x[0] != "#", u))
+        elif [(k) for k in u if k.startswith("/")]:
+            rm = " ".join(filter(lambda x: x[0] != "/", u))
+        elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+            rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+        else:
+            rm = msg
+            # print (rm)
+        try:
+            lan = translator.detect(rm)
+        except:
+            return
+        test = rm
+        if not "en" in lan and not lan == "":
+            try:
+                test = translator.translate(test, lang_tgt="en")
+            except:
+                return
+        # test = emoji.demojize(test.strip())
+
+        test = test.replace("daisy", "Aco")
+        test = test.replace("Daisy", "Aco")
+        response = await lunaQuery(
+            test, message.from_user.id if message.from_user else 0
+        )
+        response = response.replace("Aco", "Daisy")
+        response = response.replace("aco", "Daisy")
+        pro = response
+        if not "en" in lan and not lan == "":
+            try:
+                pro = translator.translate(pro, lang_tgt=lan[0])
+            except:
+                return
+        try:
+            await daisyx.send_chat_action(message.chat.id, "typing")
+            await message.reply_text(pro)
+        except CFError:
+            return
+
+
+@eugen.on_message(
+    filters.text & filters.private & ~filters.edited & filters.reply & ~filters.bot
+)
+async def inuka(client, message):
+    msg = message.text
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    u = msg.split()
+    emj = extract_emojis(msg)
+    msg = msg.replace(emj, "")
+    if (
+        [(k) for k in u if k.startswith("@")]
+        and [(k) for k in u if k.startswith("#")]
+        and [(k) for k in u if k.startswith("/")]
+        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+    ):
+
+        h = " ".join(filter(lambda x: x[0] != "@", u))
+        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+        tm = km.split()
+        jm = " ".join(filter(lambda x: x[0] != "#", tm))
+        hm = jm.split()
+        rm = " ".join(filter(lambda x: x[0] != "/", hm))
+    elif [(k) for k in u if k.startswith("@")]:
+
+        rm = " ".join(filter(lambda x: x[0] != "@", u))
+    elif [(k) for k in u if k.startswith("#")]:
+        rm = " ".join(filter(lambda x: x[0] != "#", u))
+    elif [(k) for k in u if k.startswith("/")]:
+        rm = " ".join(filter(lambda x: x[0] != "/", u))
+    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+    else:
+        rm = msg
+        # print (rm)
+    try:
+        lan = translator.detect(rm)
+    except:
+        return
+    test = rm
+    if not "en" in lan and not lan == "":
+        try:
+            test = translator.translate(test, lang_tgt="en")
+        except:
+            return
+
+    # test = emoji.demojize(test.strip())
+
+    # Kang with the credits bitches @InukaASiTH
+    test = test.replace("eugen", "Aco")
+    test = test.replace("Eugen", "Aco")
+
+    response = await lunaQuery(test, message.from_user.id if message.from_user else 0)
+    response = response.replace("Aco", "Eugen")
+    response = response.replace("aco", "Eugen")
+
+    pro = response
+    if not "en" in lan and not lan == "":
+        pro = translator.translate(pro, lang_tgt=lan[0])
+    try:
+        await daisyx.send_chat_action(message.chat.id, "typing")
+        await message.reply_text(pro)
+    except CFError:
+        return
+
+
+@daisyx.on_message(
+    filters.regex("Eugen|eugen|EugeN|EUGEN")
+    & ~filters.bot
+    & ~filters.via_bot
+    & ~filters.forwarded
+    & ~filters.reply
+    & ~filters.channel
+    & ~filters.edited
+)
+async def inuka(client, message):
+    msg = message.text
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    u = msg.split()
+    emj = extract_emojis(msg)
+    msg = msg.replace(emj, "")
+    if (
+        [(k) for k in u if k.startswith("@")]
+        and [(k) for k in u if k.startswith("#")]
+        and [(k) for k in u if k.startswith("/")]
+        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+    ):
+
+        h = " ".join(filter(lambda x: x[0] != "@", u))
+        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+        tm = km.split()
+        jm = " ".join(filter(lambda x: x[0] != "#", tm))
+        hm = jm.split()
+        rm = " ".join(filter(lambda x: x[0] != "/", hm))
+    elif [(k) for k in u if k.startswith("@")]:
+
+        rm = " ".join(filter(lambda x: x[0] != "@", u))
+    elif [(k) for k in u if k.startswith("#")]:
+        rm = " ".join(filter(lambda x: x[0] != "#", u))
+    elif [(k) for k in u if k.startswith("/")]:
+        rm = " ".join(filter(lambda x: x[0] != "/", u))
+    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+    else:
+        rm = msg
+        # print (rm)
+    try:
+        lan = translator.detect(rm)
+    except:
+        return
+    test = rm
+    if not "en" in lan and not lan == "":
+        try:
+            test = translator.translate(test, lang_tgt="en")
+        except:
+            return
+
+    # test = emoji.demojize(test.strip())
+
+    test = test.replace("eugen", "Aco")
+    test = test.replace("Eugen", "Aco")
+    response = await lunaQuery(test, message.from_user.id if message.from_user else 0)
+    response = response.replace("Aco", "Eugen")
+    response = response.replace("aco", "Eugen")
+
+    pro = response
+    if not "en" in lan and not lan == "":
+        try:
+            pro = translator.translate(pro, lang_tgt=lan[0])
+        except Exception:
+            return
+    try:
+        await daisyx.send_chat_action(message.chat.id, "typing")
+        await message.reply_text(pro)
+    except CFError:
+        return
+
+
+__help__ = """
+<b> Chatbot </b>
+DAISY AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
+
+ - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
+ - /chatbot EN : Enables English only chatbot
+ 
+ 
+<b> Assistant </b>
+ - /ask [question]: Ask question from daisy
+ - /ask [reply to voice note]: Get voice reply
+ 
+"""
+
+__mod_name__ = "AI Assistant"
