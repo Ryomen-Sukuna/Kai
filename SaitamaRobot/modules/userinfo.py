7850df57e0ece57a1ae60b4b6831b4dcf7ca62d5
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-31 02:59:39 +0700

    change little on userinfo
    Signed-off by: Ryomen-Sukuna <ryomensukun83@gmail.com>

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -370,7 +370,6 @@ def about_me(update: Update, context: CallbackContext):
         update.effective_message.reply_text(
             f"*{user.first_name}*:\n{escape_markdown(info)}",
             parse_mode=ParseMode.MARKDOWN,
-            disable_web_page_preview=True,
         )
     elif message.reply_to_message:
         username = message.reply_to_message.from_user.first_name
@@ -435,7 +434,6 @@ def about_bio(update: Update, context: CallbackContext):
         update.effective_message.reply_text(
             "*{}*:\n{}".format(user.first_name, escape_markdown(info)),
             parse_mode=ParseMode.MARKDOWN,
-            disable_web_page_preview=True,
         )
     elif message.reply_to_message:
         username = user.first_name
@@ -519,7 +517,7 @@ Examples:
  `/me @username(defaults to yours if no user specified)`
 
 *Information others add on you:*
- • `/bio`*:* will get your or another user's bio. This cannot be set by yourself.
+• `/bio`*:* will get your or another user's bio. This cannot be set by yourself.
 • `/setbio <text>`*:* while replying, will save another user's bio
 Examples:
  `/bio @username(defaults to yours if not specified).`
