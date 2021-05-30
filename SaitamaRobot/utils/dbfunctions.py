867b925f7e6ef234e16ec6728b9df0f1e766864f
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-30 12:25:39 +0700

    F

--- SaitamaRobot/utils/dbfunctions.py
+++ SaitamaRobot/utils/dbfunctions.py
@@ -3,6 +3,25 @@ from typing import Dict, List, Union
 
 karmadb = db.karma
 
+async def int_to_alpha(user_id: int) -> str:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    text = ""
+    user_id = str(user_id)
+    for i in user_id:
+        text += alphabet[int(i)]
+    return text
+
+
+async def alpha_to_int(user_id_alphabet: str) -> int:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    user_id = ""
+    for i in user_id_alphabet:
+        index = alphabet.index(i)
+        user_id += str(index)
+    user_id = int(user_id)
+    return user_id
+
+
 async def get_karmas_count() -> dict:
     chats = karmadb.find({"chat_id": {"$lt": 0}})
     if not chats:
