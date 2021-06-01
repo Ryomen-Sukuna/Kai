0978a3e3bf610bec3db56e40d278fe7570a86161
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-06-01 15:04:47 +0700

    Miss

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -82,7 +82,7 @@ def hpmanager(user):
             afkst = afk_reason(user.id)
             # if user is afk and no reason then decrease 7%
             # else if reason exist decrease 5%
-            if not afkst.reason:
+            if not afkst:
                 new_hp -= no_by_per(total_hp, 7)
             else:
                 new_hp -= no_by_per(total_hp, 5)
