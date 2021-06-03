17d169205356776d79bb8a2846c3e09735adf6ce
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-06-03 10:20:22 +0700

    help menu

--- SaitamaRobot/modules/karma.py
+++ SaitamaRobot/modules/karma.py
@@ -174,11 +174,10 @@ async def captcha_state(_, message):
         await message.reply_text(usage)
 
 
-__help__ = """
-[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
+__mod_name__ = "Karma"
+__help__ = """ [UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
 [DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
 /karma_toggle [ON|OFF] - Enable or Disable Karma System In Your Chat.
 Reply to a message with /karma to check a user's karma
 Send /karma without replying to any message to chek karma list of top 10 users
 """
-__mod_name__ = "Karma"
