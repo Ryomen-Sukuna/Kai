9ee4dc095931823ed42c954e8ad33734bd9f53e7
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-10 07:29:17 +0700

    back button

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -163,7 +163,9 @@ def send_help(chat_id, text, keyboard=None):
     """
 
     if not keyboard:
-        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
+        kb = paginate_modules(0, HELPABLE, "help")
+        kb.append([InlineKeyboardButton(text="🔙 Back", callback_data="start_back")])
+        keyboard = InlineKeyboardMarkup(kb)
     dispatcher.bot.send_message(
         chat_id=chat_id, text=text, parse_mode=ParseMode.MARKDOWN, reply_markup=keyboard
     )
