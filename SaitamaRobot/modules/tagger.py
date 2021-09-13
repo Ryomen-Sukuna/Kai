8790a36dbe16ddd63ad50cde20e6d164d4f45c4c
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-14 03:20:41 +0700

    redesign help menu

--- SaitamaRobot/modules/tagger.py
+++ SaitamaRobot/modules/tagger.py
@@ -255,16 +255,17 @@ def untagall(update, context):
 __mod_name__ = "Tagger"
 
 __help__ = """ 
-Tagger is an essential feature to mention all subscribed members in the group. Any chat members can subscribe to tagger.
+*Tagger* is an essential feature to mention all subscribed members in the group. Any chat members can subscribe to tagger.
 
-- /tagme: registers to the chat tag list.
-- /untagme: unsubscribes from the chat tag list.
+*Users Commands:*
+>> /tagme: registers to the chat tag list.
+>> /untagme: unsubscribes from the chat tag list.
 
-*Admin only:*
-- /tagall: mention all subscribed members.
-- /untagall: clears all subscribed members. 
-- /addtag <userhandle>: add a user to chat tag list. (via handle, or reply)
-- /removetag <userhandle>: remove a user to chat tag list. (via handle, or reply)
+*Admin Commands:*
+>> /tagall: mention all subscribed members.
+>> /untagall: clears all subscribed members. 
+>> /addtag <userhandle>: add a user to chat tag list. (via handle, or reply)
+>> /removetag <userhandle>: remove a user to chat tag list. (via handle, or reply)
 """
 
 TAG_ALL_HANDLER = DisableAbleCommandHandler(
