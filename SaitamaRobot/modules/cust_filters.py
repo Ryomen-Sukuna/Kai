30c11d9c52d8b380131ef699e5ec513c92f07ab9
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-14 02:25:21 +0700

    redesign help menu

--- SaitamaRobot/modules/cust_filters.py
+++ SaitamaRobot/modules/cust_filters.py
@@ -582,29 +582,31 @@ def __chat_settings__(chat_id, _):
 
 
 __help__ = """
- ✶ `/filters`*:* List all active filters saved in the chat.
+Make your chat more lively with filters; The bot will reply to certain words! can be used to create your own commands, if desired.
 
-*Admin only:*
- ✶ `/filter <keyword> <reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+*Users Commands:*
+>> /filters: List all active filters saved in the chat.
+
+*Admin Commands:*
+>> /filter <keyword> <reply message>: Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
- Separate diff replies by `%%%` to get random replies
- *Example:* 
- `/filter "filtername"
- Reply 1
- %%%
- Reply 2
- %%%
- Reply 3`
- ✶ `/stop <filter keyword>`*:* Stop that filter.
+Separate diff replies by `%%%` to get random replies
+*Example:* 
+`/filter "filtername"
+Reply 1
+%%%
+Reply 2
+%%%
+Reply 3`
+>> /stop <filter keyword>`*:* Stop that filter.
 
 *Chat creator only:*
- ✶ `/removeallfilters`*:* Remove all chat filters at once.
+>> /removeallfilters: Remove all chat filters at once.
 
-*Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
+*NOTE:* Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
 Check `/markdownhelp` to know more!
-
 """
 
 __mod_name__ = "Filters"
