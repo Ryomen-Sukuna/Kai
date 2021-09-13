244072fc3b2a4538924793bf72fd588b55a18c7c
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-14 02:07:56 +0700

    remove donation on help menu

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -94,10 +94,9 @@ HELP_STRINGS = """
 
 ➛ /help: PM's you this message.
 ➛ /help <module name>: PM's you info about that module.
-➛ /donate: information on how to donate!
 ➛ /settings:
-   ❂ in PM: will send you your settings for all supported modules.
-   ❂ in a group: will redirect you to pm, with all that chat's settings.
+   × in PM: will send you your settings for all supported modules.
+   × in a group: will redirect you to PM, with all that chat's settings.
 """
 
 
