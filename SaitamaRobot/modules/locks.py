00611db2ad3eb2a8d8f0a8134707d7c7db20713d
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-14 02:54:38 +0700

    redesign help menu

--- SaitamaRobot/modules/locks.py
+++ SaitamaRobot/modules/locks.py
@@ -563,12 +563,13 @@ You're in the right place!
 The locks module allows you to lock away some common items in the \
 telegram world; the bot will automatically delete them!
 
- • `/locktypes`*:* Lists all possible locktypes
+*Users Commands:*
+>> /locktypes: Lists all possible locktypes
 
-*Admins only:*
- • `/lock <type>`*:* Lock items of a certain type (not available in private)
- • `/unlock <type>`*:* Unlock items of a certain type (not available in private)
- • `/locks`*:* The current list of locks in this chat.
+*Admins Commands:*
+>> /lock <type>: Lock items of a certain type (not available in private)
+>> /unlock <type>: Unlock items of a certain type (not available in private)
+>> /locks: The current list of locks in this chat.
 
 Locks can be used to restrict a group's users.
 eg:
@@ -576,9 +577,9 @@ Locking urls will auto-delete all messages with urls, locking stickers will rest
 non-admin users from sending stickers, etc.
 Locking bots will stop non-admins from adding bots to the chat.
 
-*Note:*
- • Unlocking permission *info* will allow members (non-admins) to change the group information, such as the description or the group name
- • Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
+*NOTE:*
+>> Unlocking permission *info* will allow members (non-admins) to change the group information, such as the description or the group name
+>> Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
 __mod_name__ = "Locks"
