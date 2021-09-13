f8a418fe6a5c4e061557e7f95831ea9bd98da8ac
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-14 04:32:26 +0700

    MongoDB Directory

---
+++ SaitamaRobot/modules/no_sql/__init__.py
@@ -0,0 +1,28 @@
+"""MongoDB Database."""
+
+from odmantic import AIOEngine
+from motor import motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import MongoClient, collection
+from pymongo.errors import ServerSelectionTimeoutError
+from SaitamaRobot import (
+    LOGGER,
+    MONGO_URI,
+    MONGO_DB, 
+    MONGO_PORT,
+)
+
+LOGGER.info("Connecting to MongoDB")
+LOGGER.info("MongoDB Connected")
+
+mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
+mongo_client = AsyncIOMotorClient(MONGO_URI)
+db = mongo_client.KaiRobot
+engine = AIOEngine(mongo_client, MONGO_DB)
+DB_CLIENT = MongoClient(MONGO_URI)
+_DB = DB_CLIENT["KaiRobot"]
+
+
+def get_collection(name: str) -> collection:
+    """Get the collection from database."""
+    return _DB[name]
