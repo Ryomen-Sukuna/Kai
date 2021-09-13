79f019747c5ff684d8c865c4d3dfba523ba72f37
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-14 02:39:52 +0700

    redesign help menu

--- SaitamaRobot/modules/global_bans.py
+++ SaitamaRobot/modules/global_bans.py
@@ -531,18 +531,18 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = f"""
-*Admins only:*
- • `/antispam <on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
-
 Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect \
 you and your groups by removing spam flooders as quickly as possible.
-*Note:* Users can appeal gbans or report spammers at @{SUPPORT_CHAT}
 
+*Admins only:*
+>> /antispam <on/off/yes/no>: Will toggle our antispam tech or return your current settings.
 This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
+*NOTE:* Users can appeal gbans or report spammers at @{SUPPORT_CHAT}
+
 *What is SpamWatch?*
 SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
 Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
-*Note:* Users can appeal spamwatch bans at @SpamwatchSupport
+*NOTE:* Users can appeal spamwatch bans at @SpamwatchSupport
 """
 
 GBAN_HANDLER = CommandHandler("gban", gban, run_async=True)
