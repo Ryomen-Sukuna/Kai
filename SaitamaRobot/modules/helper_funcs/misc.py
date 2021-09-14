58aef89fac4ba9297a4ea15ce819bc9a0fa7f6b7
Ryomen-Sukuna <Ryomen-Sukuna@users.noreply.github.com>
2021-09-14 13:19:34 +0000

    Format code using black

--- SaitamaRobot/modules/helper_funcs/misc.py
+++ SaitamaRobot/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2]:
         pairs.append((modules[-1],))
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="kai_back")]]    
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="kai_back")]]
     return pairs
 
 
