101954cdae1c3e91e01ccb5a2c5c9c88dbe4b1c9
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-09-15 03:53:09 +0700

    misc: change
    - wall: add msg reply when wall api not found
    
    Signed-off-by: UserLazy <UserLazy@users.noreply.github.com>

--- SaitamaRobot/modules/misc.py
+++ SaitamaRobot/modules/misc.py
@@ -6,7 +6,7 @@ from typing import List
 from random import randint
 from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot import dispatcher, WALL_API
+from SaitamaRobot import dispatcher, WALL_API, SUPPORT_CHAT
 import requests as r
 import wikipedia
 from requests import get, post
@@ -149,44 +149,46 @@ def wiki(update: Update, context: CallbackContext):
 def wall(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     msg = update.effective_message
-    msg_id = update.effective_message.message_id
     args = context.args
+    msg_id = update.effective_message.message_id
+    bot = context.bot
     query = " ".join(args)
     if not query:
         msg.reply_text("Please enter a query!")
         return
-    caption = query
-    term = query.replace(" ", "%20")
-    json_rep = r.get(
-        f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}"
-    ).json()
-    if not json_rep.get("success"):
-        msg.reply_text("An error occurred!")
-
     else:
-        wallpapers = json_rep.get("wallpapers")
-        if not wallpapers:
-            msg.reply_text("No results found! Refine your search.")
-            return
-        index = randint(0, len(wallpapers) - 1)  # Choose random index
-        wallpaper = wallpapers[index]
-        wallpaper = wallpaper.get("url_image")
-        wallpaper = wallpaper.replace("\\", "")
-        context.bot.send_photo(
-            chat_id,
-            photo=wallpaper,
-            caption="Preview",
-            reply_to_message_id=msg_id,
-            timeout=60,
-        )
-        context.bot.send_document(
-            chat_id,
-            document=wallpaper,
-            filename="wallpaper",
-            caption=caption,
-            reply_to_message_id=msg_id,
-            timeout=60,
-        )
+        caption = query
+        term = query.replace(" ", "%20")
+        json_rep = r.get(
+            f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}"
+        ).json()
+        if not json_rep.get("success"):
+            msg.reply_text(f"An error occurred! Report this @{SUPPORT_CHAT}")
+        else:
+            wallpapers = json_rep.get("wallpapers")
+            if not wallpapers:
+                msg.reply_text("No results found! Refine your search.")
+                return
+            else:
+                index = randint(0, len(wallpapers) - 1)  # Choose random index
+                wallpaper = wallpapers[index]
+                wallpaper = wallpaper.get("url_image")
+                wallpaper = wallpaper.replace("\\", "")
+                bot.send_photo(
+                    chat_id,
+                    photo=wallpaper,
+                    caption="Preview",
+                    reply_to_message_id=msg_id,
+                    timeout=60,
+                )
+                bot.send_document(
+                    chat_id,
+                    document=wallpaper,
+                    filename="wallpaper",
+                    caption=caption,
+                    reply_to_message_id=msg_id,
+                    timeout=60,
+                )
 
 
 @typing_action
