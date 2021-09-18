7a9a29ecb93f8cefa00571e32f4d7bc9db17ca7b
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-19 02:57:54 +0700

    modules: use decorator handler

--- SaitamaRobot/modules/modules.py
+++ SaitamaRobot/modules/modules.py
@@ -15,9 +15,11 @@ from SaitamaRobot.__main__ import (
 )
 from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, CommandHandler
+from telegram.ext import CallbackContext
+from SaitamaRobot.modules.helper_funcs.decorators import kaicmd
 
 
+@kaicmd(command="load")
 @dev_plus
 def load(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -88,6 +90,7 @@ def load(update: Update, context: CallbackContext):
     )
 
 
+@kaicmd(command="unload")
 @dev_plus
 def unload(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -159,6 +162,7 @@ def unload(update: Update, context: CallbackContext):
     )
 
 
+@kaicmd(command="listmodules")
 @sudo_plus
 def listmodules(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -173,13 +177,5 @@ def listmodules(update: Update, context: CallbackContext):
     module_list = "Following modules are loaded : \n\n" + "".join(module_list)
     message.reply_text(module_list, parse_mode=ParseMode.HTML)
 
-
-LOAD_HANDLER = CommandHandler("load", load, run_async=True)
-UNLOAD_HANDLER = CommandHandler("unload", unload, run_async=True)
-LISTMODULES_HANDLER = CommandHandler("listmodules", listmodules, run_async=True)
-
-dispatcher.add_handler(LOAD_HANDLER)
-dispatcher.add_handler(UNLOAD_HANDLER)
-dispatcher.add_handler(LISTMODULES_HANDLER)
 
 __mod_name__ = "Modules"
