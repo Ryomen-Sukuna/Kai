4136ecb1e70bc18c3681da9f068b29ed4002a911
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-26 21:14:58 +0700

    cust_filters: done

--- SaitamaRobot/modules/cust_filters.py
+++ SaitamaRobot/modules/cust_filters.py
@@ -388,7 +388,7 @@ def reply_filter(update, context):
                     ENUM_FUNC_MAP[filt.file_type](
                         chat.id,
                         filt.file_id,
-                        caption=filtext,
+                        caption=markdown_to_html(filtext),
                         reply_to_message_id=message.message_id,
                         parse_mode=ParseMode.HTML,
                         reply_markup=keyboard,
