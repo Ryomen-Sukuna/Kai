ef2c5eeb0ca23a5984ece95ba6587a1f71f1de18
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-26 18:01:27 +0530

    `disable_web_page_preview` Removed

--- SaitamaRobot/modules/cust_filters.py
+++ SaitamaRobot/modules/cust_filters.py
@@ -355,7 +355,6 @@ def reply_filter(update, context):
                             markdown_to_html(filtext),
                             reply_to_message_id=message.message_id,
                             parse_mode=ParseMode.HTML,
-                            disable_web_page_preview=True,
                             reply_markup=keyboard,
                         )
                     except BadRequest as excp:
@@ -392,7 +391,6 @@ def reply_filter(update, context):
                         caption=markdown_to_html(filtext),
                         reply_to_message_id=message.message_id,
                         parse_mode=ParseMode.HTML,
-                        disable_web_page_preview=True,
                         reply_markup=keyboard,
                     )
                 break
